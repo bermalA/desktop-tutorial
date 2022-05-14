463dc235abc22b3adbb7bd8366768757e1093417
bermalA <101790154+bermalA@users.noreply.github.com>
2022-05-14 18:02:45 +0300

    update-1

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-function toggler(){
-    var btn =  document.getElementById("toggle");
-    if( btn.className === "dropdown-menuz"){
+function toggler() {
+    var btn = document.getElementById("toggle");
+    if (btn.className === "dropdown-menuz") {
         btn.className = "dropdown-menu";
-    }else{
+    } else {
         btn.className = "dropdown-menuz";
     }
 }
-function forDown(){
+
+function forDown() {
     var downbtn = document.getElementById("downAnchor");
-    switch(downbtn.href){
-        case "#1":
-            window.location.href="#2";
-            break;
+    for (;;) {
+        if (downbtn.href === "#1") {
+            window.location.href = "#2";
+            console.log(downbtn.href);
+        } else if (downbtn.href === "#2") {
+            window.location.href = "#3";
+        } else {
+            window.location.href = "#1";
+            downbtn.href = "#1";
+        }
     }
+}
+
+function gotothree() {
+    window.location.href = "#3"
 }
